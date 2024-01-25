93c3aa4f1e4673f7b1c5369eb791ed8ea8a0b895
dsahoo <debasish.sahoo@nttdata.com>
2024-01-25 09:09:40 -0500

    Copied from tf-module-skeleton

---
+++ tests/testimpl/test_impl.go
@@ -0,0 +1,35 @@
+package common
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/nexient-llc/lcaf-component-terratest-common/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVnetLink(t *testing.T, ctx types.TestContext) {
+	t.Run("TestAlwaysSucceeds", func(t *testing.T) {
+		assert.Equal(t, "foo", "foo", "Should always be the same!")
+		assert.NotEqual(t, "foo", "bar", "Should never be the same!")
+	})
+
+	// When cloning the skeleton to a new module, you will need to change the below test
+	// to meet your needs and add any new tests that apply to your situation.
+	t.Run("TestVnetLink", func(t *testing.T) {
+		output := terraform.Output(t, ctx.TerratestTerraformOptions, "string")
+
+		// Output contains only alphanumeric characters and 🍰
+		assert.Regexp(t, regexp.MustCompile("^[A-Za-z🍰0-9]+$"), output)
+
+		// Other tests would go here and can use functions from lcaf-component-terratest-common.
+		// Examples (from lambda):
+		// functionName := terraform.Output(t, ctx.TerratestTerraformOptions, "function_name")
+		// require.NotEmpty(t, functionName, "name of deployed lambda should be set")
+		// awsApiLambdaClient := test_helper_lambda.GetAWSApiLambdaClient(t)
+		// test_helper_lambda.WaitForLambdaSpinUp(t, awsApiLambdaClient, functionName)
+		// test_helper_lambda.TestIsLambdaInvokable(t, awsApiLambdaClient, functionName)
+		// test_helper_lambda.TestLambdaTags(t, awsApiLambdaClient, functionName, ctx.TestConfig.(*ThisTFModuleConfig).Tags)
+	})
+}
